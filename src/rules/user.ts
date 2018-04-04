090c5620c3d7f05c9b454e8662ae468e3104c862
Fin Reinhard <fin.reinhard@icloud.com>
2018-04-04 18:19:07 +0200

    fixed request validator custom validation

--- src/rules/user.ts
+++ src/rules/user.ts
@@ -3,23 +3,56 @@ import { user } from '../database/model/user';
 import { check } from '../middleware/request-validator';
 
 export const userRules = {
-    forRegister: [
-        check('mail', 'mail', mail =>
-                user.count({ where: { mail } }).then(count => !count)
-            , 'Die Mail ist bereits registriert.'),
-        check('username', 'username', username =>
-                user.count({ where: { username } }).then(count => !count)
-            , 'Der Nutzername ist leider schon vergeben.'),
-        check('password', 'password')
-    ],
-    forLogin: [
-        check('mail', 'mail', mail =>
-                user.count({ where: { mail } }).then(count => count)
-            , 'Die Mailadresse oder das Passwort ist falsch.'),
-        check('password', 'password', (password, { req }) =>
-                user.findOne({ where: { mail: req.body.mail } })
-                    .then(user => bcrypt.compare(password, user!.password))
-            , 'Die Mailadresse oder das Passwort ist falsch.'
-        )
-    ]
-};
+        forRegister: [
+            check('mail', 'mail', mail => new Promise((resolve, reject) =>
+                    user.count({ where: { mail } }).then((count) => {
+                        if (count === 0) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }))
+                , 'Die Mail ist bereits registriert.'),
+            check('username', 'username', username => new Promise((resolve, reject) =>
+                    user.count({ where: { username } }).then((count) => {
+                        if (count === 0) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }))
+                , 'Der Nutzername ist leider schon vergeben.'),
+            check('password', 'password')
+        ],
+        forLogin: [
+            check('mail', 'mail', mail => new Promise((resolve, reject) =>
+                    user.count({ where: { mail } }).then((count) => {
+                        if (count === 1) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }))
+                , 'Die Mailadresse oder das Passwort ist falsch.'),
+            check('password', 'password', (password, { req }) =>
+                    new Promise((resolve, reject) =>
+                        user.findOne({
+                            where: { mail: req.body.mail },
+                            attributes: ['password']
+                        })
+                            .then((user) => {
+                                bcrypt.compare(password, user.password)
+                                    .then((success) => {
+                                        if (success) {
+                                            resolve();
+                                        } else {
+                                            reject();
+                                        }
+                                    });
+                            })
+                    )
+                , 'Die Mailadresse oder das Passwort ist falsch.'
+            )
+        ]
+    }
+;
